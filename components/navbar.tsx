22e61787f1c1595d31bb3996773c26b0ec01028e
ZainAli <chzainali6363@gmail.com>
2024-04-21 12:51:59 +0500

    complete✨ => Sidebar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,11 +3,12 @@
 import Link from "next/link";
 import {Poppins} from "next/font/google";
 import {UserButton} from "@clerk/nextjs";
-import {Menu, Sparkles} from "lucide-react";
+import {Sparkles} from "lucide-react";
 
 import {cn} from "@/lib/utils";
 import {Button} from "@/components/ui/button";
 import {ModeToggle} from "@/components/theme-toggle";
+import {MobileSidebar} from "@/components/mobile-sidebar";
 
 const font = Poppins({
     weight: "600",
@@ -16,9 +17,9 @@ const font = Poppins({
 
 export const Navbar = () => {
     return (
-        <nav className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary">
+        <nav className="fixed w-full h-16 z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary">
             <div className="flex items-center">
-                <Menu className="block md:hidden" />
+                <MobileSidebar />
                 <Link href="/">
                     <h1 className={cn(
                         "hidden md:block text-xl md:text-3xl font-bold text-primary",
