a5ad3907d036f5b13da0b78e9448108e4f932b22
ZainAli <chzainali6363@gmail.com>
2024-04-21 12:18:29 +0500

    complete✨ => Navbar

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,9 +1,8 @@
-import {UserButton} from "@clerk/nextjs";
 
 export default function Home() {
     return (
         <div>
-            <UserButton />
+            <h1>Home</h1>
         </div>
     );
 }
