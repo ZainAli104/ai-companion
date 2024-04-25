ff08a31add64ab6f4993dbd114513324dffbc372
ZainAli <chzainali6363@gmail.com>
2024-04-25 12:28:21 +0500

    add✨ => categories filter

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,9 +1,16 @@
+import prismadb from "@/lib/prismadb";
 import {SearchInput} from "@/components/search-input";
+import {Categories} from "@/components/categories";
 
-export default function Home() {
+const RootPage = async () => {
+    const categories = await prismadb.category.findMany();
+
     return (
         <div className="h-full p-4 space-y-2">
             <SearchInput />
+            <Categories data={categories} />
         </div>
     );
 };
+
+export default RootPage;
