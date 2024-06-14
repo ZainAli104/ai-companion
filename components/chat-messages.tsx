1d8536fb004c985b3972de39949b4e86fbfc1206
ZainAli <chzainali6363@gmail.com>
2024-06-14 12:07:04 +0500

    fix => companion avatar not show in chat page

--- components/chat-messages.tsx
+++ components/chat-messages.tsx
@@ -43,7 +43,7 @@ export const ChatMessages = ({messages = [], isLoading, companion}: ChatMessages
                     key={message.content}
                     role={message.role}
                     content={message.content}
-                    src={message.src}
+                    src={companion.src}
                 />
             ))}
             {isLoading && (
