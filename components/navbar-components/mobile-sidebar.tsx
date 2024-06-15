f9e4cfda37bd0ed5fb9857f09e7bee3e5b2dd531
ZainAli <chzainali6363@gmail.com>
2024-06-15 18:44:46 +0500

    fix => props passing

--- components/navbar-components/mobile-sidebar.tsx
+++ components/navbar-components/mobile-sidebar.tsx
@@ -3,14 +3,18 @@ import {Menu} from "lucide-react";
 import {Sheet, SheetContent, SheetTrigger} from "@/components/ui/sheet";
 import {Sidebar} from "@/components/navbar-components/sidebar";
 
-export const MobileSidebar = () => {
+interface MobileSideBarProps {
+    isPro: boolean;
+}
+
+export const MobileSidebar = ({isPro}: MobileSideBarProps) => {
     return (
         <Sheet>
             <SheetTrigger className="md:hidden pr-4">
                 <Menu />
             </SheetTrigger>
             <SheetContent side="left" className="p-0 bg-secondary pt-10 w-32">
-                <Sidebar />
+                <Sidebar isPro={isPro} />
             </SheetContent>
         </Sheet>
     );
--- components/navbar-components/navbar.tsx
+++ components/navbar-components/navbar.tsx
@@ -27,7 +27,7 @@ export const Navbar = ({isPro}: NavbarProps) => {
         <nav
             className="fixed w-full h-16 z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary">
             <div className="flex items-center">
-                <MobileSidebar/>
+                <MobileSidebar isPro={isPro} />
                 <Link href="/public">
                     <h1 className={cn(
                         "hidden md:block text-xl md:text-3xl font-bold text-primary",
